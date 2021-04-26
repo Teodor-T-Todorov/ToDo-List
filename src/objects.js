f5a7b83d3d8dd72aaf3fc825b340fc0d9be6e245
TheodorTodorov <teodor.t.todorov@outlook.com>
2021-04-26 23:55:27 +0300

    Added class Todo.

---
+++ src/objects.js
@@ -0,0 +1,10 @@
+class Todo{
+    constructor(title, project, priority, description){
+        this.title = title;
+        this.project = project;
+        this.priority = priority;
+        this.description = description;
+    }
+}
+
+export default Todo;
