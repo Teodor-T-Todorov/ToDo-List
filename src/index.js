964a67223ac21f18a75cb6e3ee7bb56ba5ebd8f7
TheodorTodorov <teodor.t.todorov@outlook.com>
2021-04-30 16:23:08 +0300

    Added functionality to complete, delete todos.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import displayTodo from './DOM'
+import {addTodo, displayTodo} from './DOM'
 import Todo from './objects'
+import notie from 'notie'
 
 const navbar = document.querySelector('.navbar-container');
 const popup = document.querySelector('.popup');
+const btnAdd = document.querySelector('#btnAdd');
+const btnEdit = document.querySelector('#btnEdit');
+const container = document.querySelector('.container');
+
+displayTodo();
 
 navbar.addEventListener('click', (e)=>{ 
 
-    // Show pop-up
+    // Show pop-up to add
     if(e.target.classList.contains('add') || e.target.className == 'link-text')
     {
         popup.style.display = 'flex';
+        btnAdd.style.display = 'block';
+
     }
     
     else if(e.target.className == 'projects')
@@ -20,17 +28,65 @@ navbar.addEventListener('click', (e)=>{
 
 popup.addEventListener('click', (e)=>{
     // Close pop-up
-    if(e.target.id == 'popup')
+    if(e.target.className == 'popup')
     {
         popup.style.display = 'none';
+        btnAdd.style.display = 'none';
+        btnEdit.style.display = 'none';
+        
     }
 
+    //Add and display todo
     if(e.target.className == 'addtodo')
     {
-        // Display todo
-        let todo = new Todo('kurec', 'aa', 'dd','dd');
-        displayTodo(todo);
+        const title = document.querySelector('#todoInput');
+        const project = document.querySelector('#projectInput');
+        const description = document.querySelector('#descriptionInput');
+        const priority = document.querySelector('.priorities');
         
-        // Add to database
+        const todo = new Todo(title.value, project.value, description.value, priority.value);
+
+        addTodo(todo);
+        popup.style.display = 'none';
     }
 })
+
+container.addEventListener('click', (e)=>{
+    //Delete todo
+    if(e.target.className == 'far fa-trash-alt')
+    {
+        notie.confirm({
+            text: 'Do you want to delete this todo?',
+            cancelText: 'No',
+            submitCallback: () =>{
+                localStorage.removeItem(e.target.parentNode.parentNode.id - 1);
+                e.target.parentNode.parentNode.remove();
+            }
+        })
+    }
+
+    //Edit todo
+    else if(e.target.className == 'far fa-edit')
+    {
+        popup.style.display = 'flex';
+        btnEdit.style.display = 'block';
+    }
+
+    //Complete todo
+    else if(e.target.type == 'checkbox')
+    {
+        notie.confirm({
+            text: 'Complete todo?',
+            cancelText: 'No',
+            submitCallback: () => {
+                localStorage.removeItem(e.target.parentNode.parentNode.id - 1);
+                e.target.parentNode.parentNode.remove();
+            },
+            cancelCallback: () => {
+                e.target.checked = false;
+            }
+        })
+    }
+})
+
+console.log(localStorage)
