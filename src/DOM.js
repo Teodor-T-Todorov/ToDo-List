fd84abd73df5c062d1db3ee99e9991a0c9ad6600
TheodorTodorov <teodor.t.todorov@outlook.com>
2021-04-30 16:22:01 +0300

    Added function to add todos to local storage

--- src/DOM.js
+++ src/DOM.js
@@ -1,36 +1,67 @@
 const container = document.querySelector('.container');
 
-function displayTodo(object){
+const addTodo = (object)=>{
+    localStorage.setItem(`${localStorage.length}`, JSON.stringify(object))
+
     const todo = document.createElement('div');
     todo.setAttribute('class', 'todo');
+    todo.setAttribute('id', `${localStorage.length}`)
 
     // LHS
     const lhs = document.createElement('div');
-    lhs.setAttribute('class', 'lhs')
-
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    lhs.append(checkbox);
-
-    const title = document.createElement('p');
-    title.setAttribute('class', 'title-todo');
-    title.innerHTML = object.title; // ???
-    lhs.append(title);
-
+    lhs.setAttribute('class', 'lhs');
+    lhs.innerHTML = 
+    `
+        <input type="checkbox">
+        <p class="title-todo">${object.title}</p>  
+    `;
     todo.append(lhs);
 
     // RHS
     const rhs = document.createElement('div');
-    rhs.setAttribute('class', 'rhs')
-    rhs.innerHTML = `
-    <div class="rhs">
-        <i class="fas fa-flag"></i>
-        <i class="far fa-trash-alt"></i>
-    </div>`;
-
+    rhs.setAttribute('class', 'rhs');
+    rhs.innerHTML = 
+    `
+        <i class="far fa-flag title="Priority"></i>
+        <i class="far fa-trash-alt title="Delete todo"></i>
+        <i class="far fa-edit" title="Edit"></i>
+    `;
     todo.append(rhs);
 
     container.append(todo);
 }
 
-export default displayTodo;
+const displayTodo = ()=>{
+    Object.keys(localStorage).forEach((key) =>{
+        let currentObject = localStorage.getItem(key);
+
+        const todo = document.createElement('div');
+        todo.setAttribute('class', 'todo');
+        todo.setAttribute('id', `${localStorage.length}`)
+    
+        // LHS
+        const lhs = document.createElement('div');
+        lhs.setAttribute('class', 'lhs');
+        lhs.innerHTML = 
+        `
+            <input type="checkbox">
+            <p class="title-todo">${JSON.parse(currentObject).title}</p>  
+        `;
+        todo.append(lhs);
+    
+        // RHS
+        const rhs = document.createElement('div');
+        rhs.setAttribute('class', 'rhs');
+        rhs.innerHTML = 
+        `
+            <i class="far fa-flag" title="Priority"></i>
+            <i class="far fa-trash-alt" title="Delete todo"></i>
+            <i class="far fa-edit" title="Edit"></i>
+        `;
+        todo.append(rhs);
+    
+        container.append(todo);
+    })
+}
+
+export {addTodo, displayTodo};
